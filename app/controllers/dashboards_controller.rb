9976cd39fb3cdea9896cf300910ba0d467189296
Horace Chen <horacechen2@gmail.com>
2024-04-05 13:54:25 +1100

    initial commit with AUTH0 working

---
+++ app/controllers/dashboards_controller.rb
@@ -0,0 +1,7 @@
+class DashboardsController < ApplicationController
+    include Secured
+  def show
+    # session[:userinfo] was saved earlier on Auth0Controller#callback
+    @user = session[:userinfo]
+  end
+end
